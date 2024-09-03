3f9fb6619679c84edad08b56632c1b8d9e5a304e
Mateus-Galvao-de-Camargo <mateus.galvao.camargo.contato@gmail.com>
2024-09-03 17:38:10 -0300

    ADD CRUD Cartão

---
+++ TesteDaUmbler/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TesteDaUmbler.Models;
+
+namespace TesteDaUmbler.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Cartao> Cartoes { get; set; } = null!;
+    }
+}
---
+++ TesteDaUmbler/Models/Cartao.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TesteDaUmbler.Models
+{
+    public class Cartao
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int NumeroDoCartao { get; set; }
+
+        public int Cvc { get; set; }
+
+        public DateTime Validade { get; set; }
+
+        public string NomeNoCartao { get; set; } = null!;
+    }
+}
