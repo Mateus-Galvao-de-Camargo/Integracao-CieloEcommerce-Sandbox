8db7279e95c4cabed2047a06bdbb536723cac7d9
Mateus-Galvao-de-Camargo <mateus.galvao.camargo.contato@gmail.com>
2024-09-05 00:07:58 -0300

    ADD validação de campos, alterado o nome CVC para CVV

--- TesteDaUmbler/Models/Cartao.cs
+++ TesteDaUmbler/Models/Cartao.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace TesteDaUmbler.Models
 {
@@ -7,12 +8,44 @@ namespace TesteDaUmbler.Models
         [Key]
         public int Id { get; set; }
 
-        public int NumeroDoCartao { get; set; }
+        [Required(ErrorMessage = "O cartão é obrigatório.")]
+        [StringLength(16, MinimumLength = 16, ErrorMessage = "O cartão deve ter 16 dígitos.")]
+        [RegularExpression(@"^[4-5]\d{15}$", ErrorMessage = "O número deve começar com 4 ou 5 e conter apenas números.")]
+        public string NumeroDoCartao { get; set; }
 
-        public int Cvc { get; set; }
+        [Required(ErrorMessage = "O código de segurança é obrigatório.")]
+        [StringLength(3, MinimumLength = 3, ErrorMessage = "O CVV deve ter 3 dígitos.")]
+        [RegularExpression(@"^\d{3}$", ErrorMessage = "Somente números são permitidos.")]
+        public string Cvv { get; set; }
 
-        public DateTime Validade { get; set; }
+        [Required(ErrorMessage = "O mês de validade é obrigatório.")]
+        [Range(1, 12, ErrorMessage = "O mês deve ser entre 1 e 12.")]
+        public int Mes { get; set; }
 
+        [Required(ErrorMessage = "O ano de validade é obrigatório.")]
+        [Range(2024, 2100, ErrorMessage = "O ano deve ser entre 2024 e 2100.")]
+        public int Ano { get; set; }
+
+        public string Validade { get; set; } = null!;
+
+        [Required(ErrorMessage = "O nome no cartão é obrigatório.")]
+        [LettersOnly]
         public string NomeNoCartao { get; set; } = null!;
+    }
+
+    public class LettersOnlyAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is string stringValue)
+            {
+                // Verifica se contém apenas letras
+                if (!Regex.IsMatch(stringValue, @"^[a-z\s]+$"))
+                {
+                    return new ValidationResult("O nome do titular deve conter apenas letras.");
+                }
+            }
+            return ValidationResult.Success;
+        }
     }
 }
---
+++ TesteDaUmbler/Models/Transacao.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TesteDaUmbler.Models
+{
+    public class Transacao
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public Cartao Cartao { get; set; } = null!;
+
+        public string EstadoDaTransacao { get; set; } = null!;
+    }
+}
