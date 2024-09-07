04ab6b098fe6f0f124a197c8018605256aa4803f
Mateus-Galvao-de-Camargo <mateus.galvao.camargo.contato@gmail.com>
2024-09-07 02:15:52 -0300

    UPDATE projeto Eu tinha feito uns 10 commits explicando tudo, mas errei em alguma configuração do Visual Studio e perdi esses commits.

---
+++ TesteDaUmbler/Models/LogDaTransacao.cs
@@ -0,0 +1,52 @@
+﻿namespace TesteDaUmbler.Models
+{
+    public class Customer
+    {
+        public string? Name { get; set; }
+    }
+
+    public class CreditCard
+    {
+        public string? CardNumber { get; set; }
+        public string? Holder { get; set; }
+        public string? ExpirationDate { get; set; }
+        public string? PaymentAccountReference { get; set; }
+    }
+
+    public class Link
+    {
+        public string? Method { get; set; }
+        public string? Rel { get; set; }
+        public string? Href { get; set; }
+    }
+
+    public class Payment
+    {
+        public string? Tid { get; set; }
+        public string? ProofOfSale { get; set; }
+        public string? AuthorizationCode { get; set; }
+        public string? SoftDescriptor { get; set; }
+        public string? Provider { get; set; }
+        public bool IsQrCode { get; set; }
+        public int Amount { get; set; }
+        public string? ReceivedDate { get; set; }
+        public int Status { get; set; }
+        public bool IsSplitted { get; set; }
+        public string ReturnMessage { get; set; } = null!;
+        public string? ReturnCode { get; set; }
+        public string PaymentId { get; set; } = null!;
+        public string? Type { get; set; }
+        public string? Currency { get; set; }
+        public string? Country { get; set; }
+        public List<Link>? Links { get; set; }
+        public CreditCard? CreditCard { get; set; }
+    }
+
+    public class LogDaTransacao
+    {
+        public string? MerchantOrderId { get; set; }
+        public Customer? Customer { get; set; }
+        public Payment Payment { get; set; } = null!;
+    
+    }
+}
--- TesteDaUmbler/Program.cs
+++ TesteDaUmbler/Program.cs
@@ -6,50 +6,33 @@ using System.Threading.Tasks;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adicionar servi�os ao cont�iner
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddDbContext<AppDbContext>(x => x.UseInMemoryDatabase("app"));
-
 builder.Services.AddQuickGridEntityFrameworkAdapter();
-
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-// Adicionar o CieloService
+// Adicionar CieloService com HttpClient
 builder.Services.AddHttpClient<CieloService>();
 
-builder
-    .Services
-    .AddHttpClient("MyAPI", client => { client.BaseAddress = new Uri("https://localhost:5035/");})
-    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
-    {
-        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-    });
+builder.Services.AddHttpClient("MyAPI", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:5035/");
+})
+.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+});
+
+builder.Services.AddControllers();
+builder.Services.AddAntiforgery();
 
 var app = builder.Build();
 
-// Testar MakePayment quando o sistema iniciar
-//using (var scope = app.Services.CreateScope())
-//{
-//    var cieloService = scope.ServiceProvider.GetRequiredService<CieloService>();
-
-//    try
-//    {
-//        // Chama o m�todo CreatePayment e exibe o resultado no console
-//        var paymentResult = await cieloService.CreatePayment();
-//        Console.WriteLine($"Resultado do pagamento: {paymentResult}");
-//    }
-//    catch (Exception ex)
-//    {
-//        Console.WriteLine($"Erro ao processar o pagamento: {ex.Message}");
-//    }
-//}
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
 }
 else
 {
@@ -60,9 +43,20 @@ else
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
+
+    endpoints.MapControllers();
+});
 
 app.Run();
